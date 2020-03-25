05b338fcfa9ff475ad5d77766c638f988258474f
Akarsh Goel <akarshgoel1997@gmail.com>
2020-03-26 02:36:55 +0530

    Updates

---
+++ precache-manifest.678752ab8bbe15c7432ff97160ded212.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f3a9a23322f5f35e0fdc0889b2408af",
+    "url": "/firstReactApp/index.html"
+  },
+  {
+    "revision": "d93b6ebe6e691017416b",
+    "url": "/firstReactApp/static/css/main.5f333a8b.chunk.css"
+  },
+  {
+    "revision": "661cbc421cce5828d663",
+    "url": "/firstReactApp/static/js/2.ea1f4297.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/firstReactApp/static/js/2.ea1f4297.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d93b6ebe6e691017416b",
+    "url": "/firstReactApp/static/js/main.6660a152.chunk.js"
+  },
+  {
+    "revision": "b346ffc64d121cf82456",
+    "url": "/firstReactApp/static/js/runtime-main.2e8761c7.js"
+  }
+]);
